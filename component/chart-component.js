48762bc44d499a1df9062ea6eaec0e9880915cb8
Tyler Johnson <mail@tyler-johnson.ca>
2016-02-20 17:47:57 -0500

    Listener corrections

--- component/chart-component.js
+++ component/chart-component.js
@@ -1,21 +1,31 @@
 const proto = Object.create(HTMLElement.prototype, {
   createdCallback: {
     value: function() {
-      this.canvas = document.createElement("canvas"),
+      this.canvas = document.createElement("canvas");
       this.ctx = this.canvas.getContext("2d");
       this.appendChild(this.canvas);
       this.setAttribute("type", this.getAttribute("type") || "Line");
       this.setAttribute("eventName", this.getAttribute("eventName") || "data");
       this.setAttribute("emitter", this.getAttribute("emitter") || "bigquery-data-converter");
+      this.rechart = rechart.bind(this);
+
       document.querySelector(this.getAttribute("emitter"))
-      .addEventListener(this.getAttribute("eventName"), rechart.bind(this));
+      .addEventListener(this.getAttribute("eventName"), this.rechart);
     }
   },
   attributeChangedCallback: {
     value: function (name, oldVal, newVal) {
       if (name === "emitter") {
-        document.querySelector(oldVal).removeEventListener(this.getAttribute("eventName"), rechart.bind(this));
-        document.querySelector(newVal).addEventListener(this.getAttribute("eventName"), rechart.bind(this));
+        var oldEl = document.querySelector(oldVal),
+        newEl = document.querySelector(newVal);
+
+        if (oldEl) {
+          oldEl.removeEventListener(this.getAttribute("eventName"), this.rechart);
+        }
+
+        if (newEl) {
+          newEl.addEventListener(this.getAttribute("eventName"), this.rechart);
+        }
       }
     }
   }
